f82ca09e6a9355b7dfde5290a76321e4001ad5e8
Shujauddin94 <shujauddin9432@gmail.com>
2026-02-04 16:59:11 +0500

    largest rectangle in hiso

---
+++ Larges Rectangle in Hiso/script.js
@@ -0,0 +1,17 @@
+// Given an array of non-negative integers representing the heights of bars in a histogram, find the largest rectangale that can be formed the histogram.
+function largestRectangleArea(heights) {
+    let stack = [];
+    let maxArea = 0;
+    heights.push(0); // Add a sentinel value to pop all remaining bars in the stack at the end
+
+    for (let i = 0; i < heights.length; i++) {
+        while (stack.length > 0 && heights[i] < heights[stack[stack.length - 1]]) {
+            const height = heights[stack.pop()];
+            const width = stack.length === 0 ? i : i - stack[stack.length - 1] - 1;
+            maxArea = Math.max(maxArea, height * width);
+        }
+        stack.push(i);
+    }
+    return maxArea;
+}
+console.log(largestRectangleArea([2,1,5,6,2,3])); // Output: 10
